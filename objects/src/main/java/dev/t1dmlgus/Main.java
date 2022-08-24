edbca9e06ce6aeb5b92189c041db6c1d8df0a2d1
이의현 <59961350+t1dmlgus@users.noreply.github.com>
2022-08-24 23:30:23 +0900

    Objects 1장  (#1)
    * feat: 티켓판매 어플리케이션 요구사항
    
    * feat: 티켓판매 어플리케이션 구현

---
+++ objects/src/main/java/dev/t1dmlgus/Main.java
@@ -0,0 +1,7 @@
+package dev.t1dmlgus;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ objects/src/main/java/dev/t1dmlgus/chapter01/Invitation.java
@@ -0,0 +1,7 @@
+package dev.t1dmlgus.chapter01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
