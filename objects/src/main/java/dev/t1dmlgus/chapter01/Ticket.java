0ff0985dac7f5610edf56d71f4ea0d0f7706dac5
t1dmlgus <dmlgusgngl@gmail.com>
2022-08-24 23:29:14 +0900

    feat: 티켓판매 어플리케이션 구현

---
+++ objects/src/main/java/dev/t1dmlgus/chapter01/Ticket.java
@@ -0,0 +1,9 @@
+package dev.t1dmlgus.chapter01;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
---
+++ objects/src/main/java/dev/t1dmlgus/chapter01/TicketSeller.java
@@ -0,0 +1,15 @@
+package dev.t1dmlgus.chapter01;
+
+public class TicketSeller {
+
+    private final TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+
+    }
+}
